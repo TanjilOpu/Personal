0fe1037d5e0ae6e4a5b004486d44461acc5b468d
Md. Tanjil Islam Opu <tanjilislamopu468@gmail.com>
2024-12-23 12:59:02 +0600

    Add files via upload

---
+++ Codeforces/kevin_and_combination_lock.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+ 
+int main()
+{
+ 
+ ll tc;
+ cin>>tc;
+ while(tc--)
+ {
+    ll n;
+    cin>>n;
+    if(n%33==0)
+    {
+        cout<<"YES"<<endl;
+    }
+    else cout<<"NO"<<endl;
+
+ }
+ 
+ 
+    return 0;
+}
