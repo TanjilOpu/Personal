f3d7a880f02714527603b410e25335354d86f890
Md. Tanjil Islam Opu <tanjilislamopu468@gmail.com>
2025-04-26 19:53:07 +0600

    Add files via upload

---
+++ Codeforces/2103A_Common_Multiple.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n;
+    cin>>n;
+     set<int>st;
+     for(int i=0;i<n;i++)
+     {
+        int x;
+        cin>>x;
+        st.insert(x);
+     }
+     cout<<st.size()<<endl;
+}
+int main()
+{
+    int tc;
+    cin>>tc;
+    while(tc--)
+    {
+        solve();
+    }
+}
