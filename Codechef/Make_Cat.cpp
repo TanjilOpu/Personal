f31981d409c18a4d543c34390a96eb19db27ee87
Md. Tanjil Islam Opu <tanjilislamopu468@gmail.com>
2025-08-07 13:02:16 +0600

    Add files via upload

---
+++ Codechef/Make_Cat.cpp
@@ -0,0 +1,71 @@
+// problem link :   
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T> using order_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+#define int long long int
+#define yes cout << "YES" <<endl
+#define no cout << "NO" << endl
+#define nl '\n' 
+#define vc vector<int>
+#define pb push_back
+#define ppb pop_back
+#define inputvc(n) for(int i = 0; i < n; i++) cin>>v[i]
+#define outputvc(n) for(int i = 0; i < n; i++) cout<<v[i]<<' ' 
+#define f1(n) for (int i = 1; i <= n; i++)
+#define srtall(a) sort(a.begin(), a.end())
+#define rsrtall(a) sort(a.rbegin(), a.rend())
+const int mod = 1e9 + 7;
+#define PI acos(-1)
+#define debug(x) cerr << (#x) << " " << (x) << endl
+#define fast()                            \
+    {                                     \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(NULL);                    \
+    }
+
+int gcd(int a, int b)
+ {
+   if(b==0) return a;
+   return gcd(b,a%b);
+ }
+ 
+bool prime(int n)
+{
+    if(n < 2) return false;
+    if(n <= 3) return true;
+    if(n % 2 == 0) return false;
+    for(int i = 3; i * i <= n; i += 2)
+    {
+        if(n % i == 0) return false;
+    }
+    return true;
+}
+ 
+void solve()
+{
+   string s;
+   cin>>s;
+   int c=0,a=0,t=0;
+   for(int i=0;i<3;i++)
+   {
+       if(s[i]=='c') c++;
+       else if(s[i]=='a') a++;
+       else if(s[i]=='t') t++;
+   }
+   if(c==1 && a==1 && t==1) cout<<"Yes\n";
+   else cout<<"No"<<endl;
+}
+
+int32_t main()
+{
+    fast();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+}
---
+++ Codechef/Minimize_Maximum_Mex.cpp
@@ -0,0 +1,104 @@
+// problem link :
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using order_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define int long long int
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define nl '\n'
+#define vc vector<int>
+#define pb push_back
+#define ppb pop_back
+#define inputvc(n)              \
+    for (int i = 0; i < n; i++) \
+    cin >> v[i]
+#define outputvc(n)             \
+    for (int i = 0; i < n; i++) \
+    cout << v[i] << ' '
+#define f1(n) for (int i = 1; i <= n; i++)
+#define srtall(a) sort(a.begin(), a.end())
+#define rsrtall(a) sort(a.rbegin(), a.rend())
+const int mod = 1e9 + 7;
+#define PI acos(-1)
+#define debug(x) cerr << (#x) << " " << (x) << endl
+#define fast()                            \
+    {                                     \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(NULL);                    \
+    }
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+bool prime(int n)
+{
+    if (n < 2)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0)
+        return false;
+    for (int i = 3; i * i <= n; i += 2)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> va(n), vb(n);
+    for (int i = 0; i < n; i++)
+        cin >> va[i];
+    for (int i = 0; i < n; i++)
+        cin >> vb[i];
+        set<int>must_take,one_take;
+        bool first_occuracnce = true;
+        for(int i=0;i<n;i++)
+        {
+            if(va[i]==vb[i]) must_take.insert(va[i]);
+            else {
+                one_take.insert(va[i]);
+                one_take.insert(vb[i]);
+            }
+
+        }
+        int mex=0;
+        while(true)
+        {
+             if(must_take.count(mex))
+             {
+                mex++;
+             }
+             else if(first_occuracnce && one_take.count(mex))
+             {
+                mex++;
+                first_occuracnce = false;
+             }
+             else break;
+        }
+        cout<<mex<<endl;
+
+}
+
+int32_t main()
+{
+    fast();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+}
